0973f216e3d0c267e368b3378085536b3d7a7681
Arianna Buja <tue69402@temple.edu>
2015-11-13 17:21:52 -0500

    fuckfuckfuck

--- TermProjectWS/TheWebService.asmx.cs
+++ TermProjectWS/TheWebService.asmx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Services;
 using System.Data;
 using Utilities;
+using System.Data.SqlClient;
 
 namespace TermProjectWS
 {
@@ -25,6 +26,13 @@ namespace TermProjectWS
             DBConnect DB = new DBConnect();
             //This method returns a list of Department Numbers and Department Names from the Department table in your database. 
             //The Dataset returned by the function consists of 2 columns corresponding to the Department Number and Department Name.
+
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "GetDepartments";
+            DataSet DepDS = DB.GetDataSetUsingCmdObj(command);
+
+            return DepDS;
         }
 
         [WebMethod]
@@ -33,22 +41,36 @@ namespace TermProjectWS
             //This method accepts a Department Number as input and returns a DataSet containing the Product Number, Description, Price, 
             //and image URL for all products in that Department.  Dataset consists of 4 columns corresponding to the Product Number, 
             //Description, Price, and image URL.  
+             DBConnect DB = new DBConnect();
+             SqlCommand command = new SqlCommand();
+             command.CommandType = CommandType.StoredProcedure;
+             command.CommandText = "ProductCatalog";
+             command.Parameters.AddWithValue("@DepartmentNumber", DepartmentNumber);
+             DataSet ProdDS = DB.GetDataSetUsingCmdObj(command);
+
+            return ProdDS;
         }
 
         [WebMethod]
-        public Boolean RegisterSite(String SiteID, String Description, String APIKey, String email, Contact information)
+        public Boolean RegisterSite(String SiteID, String Description, String APIKey, String email, string contactInfo)
         {
            // This method will create a record for a site that will sell products on your behalf. You will need to store the Site’s 
             //ID and it should return true when it successfully registers a site and false when the registration fails. 
             //The APIKey was generated during the Merchant Registration process on the Amazon Site you used to create a Merchant Account 
             //(Term Project Part 1 – Section 4). This APIKey should be a unique value shared between you and the Amazon Site that sells 
             //your products.
+
+            return true;
         }
 
-        public Boolean Purchase(String ProductID, int Quantity, String SiteID, String APIKey, /*String[] Customer Information, Customer CreditCard Information*/)
-    {
+        public Boolean Purchase(String ProductID, int Quantity, String SiteID, String APIKey /*String[] Customer Information, Customer CreditCard Information*/)
+             {
 
-    }
 
+
+                 return true;
+             }
+
+       
     }
 }
