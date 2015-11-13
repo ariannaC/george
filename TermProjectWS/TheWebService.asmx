0211789800a664791a0689c285e0bc76a56ec059
Arianna Buja <tue69402@temple.edu>
2015-11-13 14:43:37 -0500

    Initial commit
    Created project

---
+++ TermProjectWS/TheWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="TheWebService.asmx.cs" Class="TermProjectWS.TheWebService" %>
---
+++ TermProjectWS/TheWebService.asmx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using Utilities;
+
+namespace TermProjectWS
+{
+    /// <summary>
+    /// Summary description for TheWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TheWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public DataSet GetDepartments()
+        {
+            DBConnect DB = new DBConnect();
+            //This method returns a list of Department Numbers and Department Names from the Department table in your database. 
+            //The Dataset returned by the function consists of 2 columns corresponding to the Department Number and Department Name.
+        }
+
+        [WebMethod]
+        public DataSet GetProductCatalog(String DepartmentNumber)
+        {
+            //This method accepts a Department Number as input and returns a DataSet containing the Product Number, Description, Price, 
+            //and image URL for all products in that Department.  Dataset consists of 4 columns corresponding to the Product Number, 
+            //Description, Price, and image URL.  
+        }
+
+        [WebMethod]
+        public Boolean RegisterSite(String SiteID, String Description, String APIKey, String email, Contact information)
+        {
+           // This method will create a record for a site that will sell products on your behalf. You will need to store the Site’s 
+            //ID and it should return true when it successfully registers a site and false when the registration fails. 
+            //The APIKey was generated during the Merchant Registration process on the Amazon Site you used to create a Merchant Account 
+            //(Term Project Part 1 – Section 4). This APIKey should be a unique value shared between you and the Amazon Site that sells 
+            //your products.
+        }
+
+        public Boolean Purchase(String ProductID, int Quantity, String SiteID, String APIKey, /*String[] Customer Information, Customer CreditCard Information*/)
+    {
+
+    }
+
+    }
+}
