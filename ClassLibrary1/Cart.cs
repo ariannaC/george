25eb6edffb2c3a8fbd88022cd2056d4db94c4fca
Lmaslanka75 <Lauren.Maslanka@temple.edu>
2015-11-15 18:34:37 -0500

    Product class and Product codebehind

---
+++ ClassLibrary1/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Amazon_ClassLibrary
+{
+    public class Cart
+    {
+            //internal list of items and constructor which instantiates it
+            private List<CartItem> cartItems;
+            public Cart()
+            {
+                cartItems = new List<CartItem>();
+            }
+
+            //readonly property that returns the number of items in internal list
+            public int Count
+            {
+                get { return cartItems.Count; }
+            }
+
+            //indexers - locate items in internal list by index or product id
+            public CartItem this[int index]
+            {
+                get { return cartItems[index]; }
+                set { cartItems[index] = value; }
+            }
+            public CartItem this[string id]
+            {
+                get
+                {
+                    return cartItems.FirstOrDefault(c => c.Product.ProductID == id);
+                }
+            }
+
+            
+        
+
+
+
+
+
+
+
+
+    }//end of Cart class
+
+
+
+
+
+
+}//end of namespace
---
+++ ClassLibrary1/CartItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Amazon_ClassLibrary
+{
+    public class CartItem
+    {
+        //create empty object or add values on creation
+        public CartItem() { }
+        public CartItem(Product product, int quantity)
+        {
+            this.Product = product;
+            this.Quantity = quantity;
+        }
+
+        //public properties
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        //add to quantity
+        public void AddQuantity(int quantity)
+        {
+            this.Quantity += quantity;
+        }
+
+        //display item's property values
+        public string Display()
+        {
+            string display = string.Format("{0} ({1} at {2} each)",
+                Product.Description, Quantity.ToString(),
+                Product.Price.ToString("c"));
+            return display;
+        }
+
+
+
+    }
+}
