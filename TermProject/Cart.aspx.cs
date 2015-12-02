c1206638398c57e82bd8579be99a29cfffded918
Arianna Buja <tue69402@temple.edu>
2015-12-02 15:18:06 -0800

    add selected product to cart finished

--- TermProject/Cart.aspx.cs
+++ TermProject/Cart.aspx.cs
@@ -18,21 +18,26 @@ namespace TermProject
 
                 if (Session["Cart"] != null)
                 {
-                    
+
                     //TP_Amazon_ClassLibrary.Product testProduct = new TP_Amazon_ClassLibrary.Product();
                     //testProduct.Description = "Test Product";
                     //testProduct.Price = 20;
                     //testProduct.merchantName = "Princess Bubblegum";
                     //CartItem testItem = new CartItem(testProduct, 5);
 
-                   
+
 
                     TP_Amazon_ClassLibrary.Cart cart = (TP_Amazon_ClassLibrary.Cart)Session["Cart"];
                     //cart.AddItem(testItem);
                     //cart.AddItem(testItem2);
                     rptCart.DataSource = cart.cartItems;
                     rptCart.DataBind();
-                   // Session["Cart"] = cart;
+                    // Session["Cart"] = cart;
+                }
+                else
+                {
+                    Label lblDisplay = new Label();
+                    lblDisplay.Text = "Cart is empty";
                 }
             }
         }
@@ -81,16 +86,16 @@ namespace TermProject
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
-            TP_Amazon_ClassLibrary.Cart cart = (TP_Amazon_ClassLibrary.Cart)Session["Cart"];
-            TP_Amazon_ClassLibrary.Product testProduct2 = new TP_Amazon_ClassLibrary.Product();
-            testProduct2.Description = "Test Product2";
-            testProduct2.Price = 20;
-            testProduct2.merchantName = "Murderface";
-            CartItem testItem2 = new CartItem(testProduct2, 5);
-            cart.AddItem(testItem2);
-            Session["Cart"] = cart;
-            rptCart.DataSource = cart.cartItems;
-            rptCart.DataBind();
+            //TP_Amazon_ClassLibrary.Cart cart = (TP_Amazon_ClassLibrary.Cart)Session["Cart"];
+            //TP_Amazon_ClassLibrary.Product testProduct2 = new TP_Amazon_ClassLibrary.Product();
+            //testProduct2.Description = "Test Product2";
+            //testProduct2.Price = 20;
+            //testProduct2.merchantName = "Murderface";
+            //CartItem testItem2 = new CartItem(testProduct2, 5);
+            //cart.AddItem(testItem2);
+            //Session["Cart"] = cart;
+            //rptCart.DataSource = cart.cartItems;
+            //rptCart.DataBind();
         }
     }
 }
